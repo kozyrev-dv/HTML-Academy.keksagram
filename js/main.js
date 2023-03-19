43758fe55c476c8991ec8df4640313e2405e7196
kozyrev-dv <k-daniilv@mail.ru>
2023-03-19 12:44:44 +0300

    array of photos generation implemented

--- js/main.js
+++ js/main.js
@@ -7,5 +7,18 @@ function randInt(a, b) {
 
 const checkLength = (message, maxLength) => String(message).length <= maxLength;
 
-randInt(2, 4);
+const generatePhoto = (id, description) => ({
+  id: id,
+  url: `photos/${id + 1}.jpg`,
+  description: description,
+  likes: randInt(15, 200),
+  comments: randInt(0, 200)
+});
+
+const generatePhotos = (amount, descriptions) => Array.from({length: amount}, (v, index) => generatePhoto(index, descriptions[index]));
+
+const PHOTOS_AMOUNT = 25;
+
+generatePhotos(PHOTOS_AMOUNT, Array.from({length: PHOTOS_AMOUNT}, () => ''));
+
 checkLength('Hello World!', 12);
