33be50b1700b024d39441718e7b18c8e66ffb109
kozyrev-dv <k-daniilv@mail.ru>
2023-03-20 11:57:22 +0300

    project is moduled. utils.js and data.js added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-function randInt(a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
-  return Math.floor(Math.random() * (upper - lower + 1) + lower);
-}
-
-const checkLength = (message, maxLength) => String(message).length <= maxLength;
-
-const generatePhoto = (id, description) => ({
-  id: id,
-  url: `photos/${id + 1}.jpg`,
-  description: description,
-  likes: randInt(15, 200),
-  comments: randInt(0, 200)
-});
-
-const generatePhotos = (amount, descriptions) => Array.from({length: amount}, (v, index) => generatePhoto(index, descriptions[index]));
+import { generatePhotos } from './data.js';
+import { checkLength } from './utils.js';
 
 const PHOTOS_AMOUNT = 25;
 const DESCRIPTIONS = Array.from({length: PHOTOS_AMOUNT}, (v, index) => `the ${index + 1} photo`);
