722857385142db44dd16ea50fbf136aab4b39228
kozyrev-dv <k-daniilv@mail.ru>
2023-04-01 12:48:35 +0300

    documentFragment optimisation for pictures rendering is done

--- js/render.js
+++ js/render.js
@@ -3,14 +3,17 @@ import { generatePhotos } from './data.js';
 const generatePhotoElements = (amount, urls, descriptions) => {
 
   const picturesContainer = document.querySelector('.pictures');
+  const picturesFragment = document.createDocumentFragment();
+
   const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
   for(const photo of generatePhotos(amount, urls, descriptions)) {
     const photoElement = photoTemplate.cloneNode(true);
     photoElement.querySelector('img').src = photo.url;
     photoElement.querySelector('.picture__comments').textContent = photo.comments;
     photoElement.querySelector('.picture__likes').textContent = photo.likes;
-    picturesContainer.appendChild(photoElement);
+    picturesFragment.appendChild(photoElement);
   }
+  picturesContainer.appendChild(picturesFragment);
 };
 
 export {generatePhotoElements};
