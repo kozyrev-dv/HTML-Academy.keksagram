167aa9982fff8b63410fec499db6f84f0a348f50
kozyrev-dv <k-daniilv@mail.ru>
2023-04-01 12:45:20 +0300

    photo elements rendering added. All render functions are separated into render.js file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-import { generatePhotos } from './data.js';
+import { generatePhotoElements } from './render.js';
 import { checkLength } from './utils.js';
 
-const PHOTOS_AMOUNT = 25;
-const DESCRIPTIONS = Array.from({length: PHOTOS_AMOUNT}, (v, index) => `the ${index + 1} photo`);
+const PHOTO_AMOUNT = 25;
+const DESCRIPTIONS = Array.from({length: PHOTO_AMOUNT}, (v, index) => `the ${index + 1} photo`);
+const PHOTO_URLS = Array.from({length: PHOTO_AMOUNT}, (V, index) => `photos/${index + 1}.jpg`);
 
-generatePhotos(PHOTOS_AMOUNT, DESCRIPTIONS);
+generatePhotoElements(PHOTO_AMOUNT, PHOTO_URLS, DESCRIPTIONS);
 
 checkLength('Hello World!', 12);
