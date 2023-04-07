dad2b65d866e3549d3eaa832daec06de64d88010
kozyrev-dv <k-daniilv@mail.ru>
2023-04-07 18:04:48 +0300

    image-upload-form module added. <form> attrs are corrected. All needed event listeners added to hide/show upload image overlay.

--- js/image-upload-form.js
+++ js/image-upload-form.js
@@ -1,8 +1,9 @@
 const HIDDEN_CSS_CLASS = 'hidden';
+const MODAL_OPEN_CSS_CLASS = 'modal-open';
 
 const IMAGE_SIZE_MAX = 100;
 const IMAGE_SIZE_MIN = 25;
-const IMAGE_SIZE_INIT = 50;
+const IMAGE_SIZE_INIT = 100;
 const IMAGE_RESIZE_STEP = 25;
 
 const fileSelectInputElement = document.querySelector('#upload-file');
@@ -14,6 +15,7 @@ const imageUploadPreview = document.querySelector('.img-upload__preview');
 const scaleControlValueElement = document.querySelector('.scale__control--value');
 
 let imageScaleValue = IMAGE_SIZE_INIT;
+let isPreviewContainerHidden = true;
 
 const setScaleControlValue = (value) => {
   imageScaleValue = imageScaleValue = Math.min(IMAGE_SIZE_MAX, Math.max(IMAGE_SIZE_MIN, value));
@@ -22,18 +24,32 @@ const setScaleControlValue = (value) => {
 };
 
 const showImagePreviewContainer = () => {
-  imageScaleValue = IMAGE_SIZE_INIT;
+  if (isPreviewContainerHidden) {
+    setScaleControlValue(IMAGE_SIZE_INIT);
+    isPreviewContainerHidden = false;
 
-  imageUploadContainer.classList.remove(HIDDEN_CSS_CLASS);
+    document.body.classList.add(MODAL_OPEN_CSS_CLASS);
+    imageUploadContainer.classList.remove(HIDDEN_CSS_CLASS);
+  }
 };
 
 const hideImagePreviewContainer = () => {
-  imageUploadContainer.classList.add(HIDDEN_CSS_CLASS);
-  fileSelectInputElement.value = '';
+  if (!isPreviewContainerHidden) {
+    imageUploadContainer.classList.add(HIDDEN_CSS_CLASS);
+    document.body.classList.remove(MODAL_OPEN_CSS_CLASS);
+
+    isPreviewContainerHidden = true;
+    fileSelectInputElement.value = '';
+  }
 };
 
 fileSelectInputElement.addEventListener('input', showImagePreviewContainer);
 uploadCancelButton.addEventListener('click', hideImagePreviewContainer);
+document.addEventListener('keydown', (evt) => {
+  if (!isPreviewContainerHidden && (evt.key === 'Esc' || evt.key === 'Escape')) {
+    hideImagePreviewContainer();
+  }
+});
 
 imageUploadSubmitButton.addEventListener('submit', () => {
   hideImagePreviewContainer();
