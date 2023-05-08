4c55d0e034664ae5647490a2833aff4208831efe
kozyrev-dv <k-daniilv@mail.ru>
2023-05-08 11:19:26 +0300

    cleared and web api copied

--- js/data.js
+++ js/data.js
@@ -11,4 +11,26 @@ const generatePhoto = (id, url, description) => ({
 const generatePhotos = (amount, urls, descriptions) => Array.from({length: amount},
   (v, index) => generatePhoto(index, urls[index], descriptions[index]));
 
+const createLoader = (onSuccess, onError) => () => fetch(
+  'https://26.javascript.pages.academy/code-and-magick/data',
+  {
+    method: 'GET',
+    credentials: 'same-origin',
+  },
+)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status} ${response.statusText}`);
+  })
+  .then((data) => {
+    onSuccess(data);
+  })
+  .catch((err) => {
+    onError(err);
+  });
+
+export {createLoader};
 export {generatePhotos};
